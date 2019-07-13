90f268bc42b7e685cc0b5697ee2eab60b70a75cf
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-07-13 12:14:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+let ginsoy = {
+
+    Home: [{
+        soups: [],
+        startr: [],
+        Beef: [],
+        seaFood: [],
+        poultry: [],
+        noodles: [],
+        rice: [],
+    }],
+    about: "Life’s good when you get to live the finest Chinese experience with top quality food served in a pleasant environment. At Ginsoy we promise you this and much more. As the “Extreme Chinese” name suggests, Ginsoy was created with an inspiration to bring unique, striking and flavorful Chinese cuisine to Pakistan.",
+    resrvation: [
+        {
+            about: 'At Ginsoy, each detail of the restaurant, from the utensils to the paint selection, has been carefully chosen to give a modern and uplifting feel to the restaurant. The stylish and relaxing decor sets the perfect backdrop for the fresh and tasty recipes that are served in contemporary tableware. The restaurant is graced with natural light during the day and waved by the dimly lit colorful lamps above each table at night.',
+            northNazimabad: '',
+            dha: '',
+            sMcHs: '',
+            lahoreOutlet: '',
+            sindhiMuslimSociety: '0213474899302211',
+            defencePhraseIv: '021347489930Ginsoy',
+            northNazimabad: '021376648900033'
+        }
+    ],
+    menu: [],
+    order: {
+
+        soups: {
+            chickenCornSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            hotAndSourSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            thaiSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            tomYumGaiSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            tomYumGoongSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            WontonNoodlesoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            schezwanSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            streetStyleChickenSoupWithSlims: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+            manchowSoup: {
+                singleServes: '1-2',
+                Rs: "265",
+                familyServes: '4-5',
+                Rs: "695",
+            },
+
+        },
+
+        starter: {
+            dynamiteChicken: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            hotTigerPrawnXL: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            prawnTempura: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            dynamitePrawns: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            prawnBalls: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            ButterflyPrawns: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            PrawOnToastWithSesameSeeds: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            ChickenDrumSticks: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+            PaperWrappedChicken: {
+                singleServes: '1-2',
+                Rs: "465",
+                familyServes: '4-5',
+                Rs: "895",
+            },
+
+        }
+
+
+    },
+    online: [],
+    feeback: [
+        {
+            Info: 'We’re glad to hear from you. Use this feedback from to provide more information about Ginsoy. You may also find the information you’re seeking in Frequently Asked Questions.',
+            name: 'Abc',
+            contact: '0478329839913',
+            email: 'abs@gmail.com',
+            message: 'hi you are amazing',
+        }
+    ],
+    carrer: [],
+    contacUs: [{
+        name: 'Abc',
+        contact: '0478329839913',
+        email: 'abs@gmail.com',
+        message: 'hi you are amazing',
+        operatingHours: [
+            {
+                Lunch: ["12:30PM", '4:00PM'],
+                Dinner: ['06:30PM ', ' 12:30AM'],
+            }
+        ],
+        forDeleiveryCallNow: [
+            {
+                dhaPhaseVi: '(021)111-GINSOY(446769)',
+                sMcHs: '(021)34398831-35',
+                northNazimabad: '021376648900033',
+                gulsantan_e_Jahore: '(021)34198787-88-89',
+                gulshan_e_IqbL: '(021)34992170-74',
+                gulbergLahore: 'Coming Soon'
+            }
+        ]
+    }],
+    location: [
+        {
+            googleMapMain: 'https://www.google.com/maps/dir//24.8640325,67.0549276/@24.864033,67.054928,11z?hl=en-US',
+            others: [],
+
+        }
+
+    ],
+
+
+}
