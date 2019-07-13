0dbe42e65b4624ade977704a7e70ebda357f7365
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-07-13 15:54:40 +0500

    Add files via upload

---
+++ teck.js
@@ -0,0 +1,26 @@
+let techkaro = {
+    name: "Tech Karo",
+    shortDescription: "A Web Dev Course",
+    longDescription: "TechKaro is an 8 months' Web Development Course, offered by CIRCLE Women Pakistan and sponsored by Engro Vopak. The course is aimed at talented but underserved youth of Pakistan. Our goal is to empower women in technology and help them generate income through their Web Development skills.",
+    category:"Education",
+    Instructor:["sarah ahmed","abc"],
+    abmasdor:[
+        {
+            name:"khier un Nisa",
+            slils:"html css  wordpress",
+            days:"tts",
+        },
+        {
+            name:"waqas",
+            slils:"html css  wordpress",
+            days:"tts",
+        },
+        {
+            name:"khier un Nisa",
+            slils:"html css  wordpress",
+            days:"tts",
+        }
+    ]
+    
+
+}
