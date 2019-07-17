84333872c7ae0bcd580259841b518e2de2661081
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-07-17 20:57:40 +0500

    Add files via upload

---
+++ UIT.js
@@ -0,0 +1,201 @@
+let university = {
+
+    logo: "https://www.uit.edu/assets/images/LogoUIT(White).png",
+    name: "Usman Institute of Technology",
+    Afilliated: "NED University of Engineering & Technology",
+    city:"Karchi",
+    Braches:"over all in pakistan",
+    address: "ST-13, Block 7, Gulshan-e-Iqbal, Abul Hasan Isphahani Road, Opposite Safari Park, Karachi 75300.",
+    contactDetails:
+    {
+        phone: ["+92 (213) 499 4305", "+92 (213) 497 6181"],
+        email: "+92 (213) 497 6181",
+    },
+    UITManagementTeam:
+        [
+
+            {
+                name: "DR. ZAHIR ALI SYED",
+                positioin: "Director",
+                status: "Learning"
+            },
+            {
+                name: "DR. SHOAIB ZAIDI",
+                positioin: "Head of Academic ",
+                status: "Learning"
+            },
+            {
+                name: "DR. ABID KARIM",
+                positioin: "Head Project based  ",
+                status: "Learning"
+            },
+            {
+                name: "DR. SYED TALHA AHSAN",
+                positioin: "HoD Electrical ",
+                status: "Engineering"
+            },
+            {
+                name: "MUHAMMAD IFTIKHAR MUBBASHIR",
+                positioin: " HoD ",
+                status: "H&BS and M&SS"
+            },
+            {
+                name: "ENGR. PARKASH LOHANA",
+                positioin: " HoD Computer  ",
+                status: "Science"
+            },
+            {
+                name: "SHABANA NAZ",
+                positioin: "Head of Academic ",
+                status: "Finance and Accounts",
+            },
+            {
+                name: "QAZI MUHAMMAD ",
+                positioin: "Head of Academic ",
+                status: " Administration and Examination"
+            }
+        ],
+    student: {
+        timeTable: "https://www.uit.edu/TimeTable.pdf",
+        handbook: "https://www.uit.edu/Student_booklet.pdf",
+        societies: "IEEE Student BranchIt has been formed to encourage participation of UIT students in various academic and technical activities like seminars and exhibitions.Also IMPETUS(IEEE Membership Plan to encourage top UIT students) has been launched, under which two best students are financed by UIT for membership of IEEE(USA) IEEEP Student BranchIEEEP Student Branch at UIT is actively engaged in students’ activities like seminars, conferences and workshops under the patronage of IEEEP. Association of Computing Machinery(ACM) Student ChapterAssociation of Computing Machinery(ACM) has officially chartered UIT ACM Student Chapter.This is an international scientific and educational organization dedicated to advancing the arts, sciences, and applications of Information Technology.The chapter provides a range of activities and services including talks by local practitioners, visits from prominent speakers, the ACM Lectureship Program circuit, technical and career workshops, field trips to computing installations, and social activities.",
+        academicCalender: "https://www.uit.edu/AcademicCalender.pdf",
+        studentAffairs: "",
+        academicRulesAndRegulations: [
+            "Admission in any Semester shall be allowed only during first three weeks after commencement of the classes of that Semester.",
+            "Admission in Fall Semester of any Year shall be open to any student. Similarly, admission in Spring Semester of any year shall be open to any student who was registered in Fall Semester of the preceding Year.",
+            "Any student who fails to obtain at least 2.0 CGPA in any Semester shall remain on probation during next Semester. In any case, if such student still obtain less than 2.0 CGPA in the subsequent semester shall remain on second probation and shall be allowed to register in the following semester"
+        ],
+        examinationSchedule: [
+            {
+                name: "Lab Exam (BS) Spring-2017",
+                at: "https://www.uit.edu/assets/downloads/LabExamBS.pdf"
+            },
+            {
+                name: "Lab Exam (EL) Spring-2017",
+                at: "https://www.uit.edu/assets/downloads/LabExamBS.pdf"
+            },
+            {
+                name: "BS DateSheet Spring-2017",
+                at: "https://www.uit.edu/assets/downloads/LabExamBS.pdf"
+            },
+            {
+                name: "EL DateSheet Spring-2017",
+                at: "https://www.uit.edu/assets/downloads/LabExamBS.pdf"
+            },
+            {
+                name: "Seating Arrangement Morning",
+                at: "https://www.uit.edu/assets/downloads/LabExamBS.pdf"
+            },
+
+        ],
+
+    },
+    academic: {
+        programmers: {
+            undergraduate: [
+
+                {
+                    name: "electronic",
+                    duration: "4 Years full time and maximum Seven years",
+                    cGpA: "2.00 in each semester without failing in any course",
+                    courses: {
+                        course: "EL111",
+                        title: "LINEAR CIRCUIT ANALYSIS",
+                        hours: "3+1",
+                    },
+                },
+                {
+                    name: "Power",
+                    duration: "4 Years full time and maximum Seven years",
+                    cGpA: "2.00 in each semester without failing in any course",
+                    courses: {
+                        course: "EL111",
+                        title: "LINEAR CIRCUIT ANALYSIS",
+                        hours: "3+1",
+                    },
+                }
+            ]
+
+        },
+        faculty: [
+            {
+                name: "DR. ZAHIR ALI SYED",
+                positioin: "Director",
+                status: "Learning",
+                image: "https://www.uit.edu/assets/images/faculty/ZahirSir.jpg"
+            },
+            {
+                name: "DR. SHOAIB ZAIDI",
+                positioin: "Head of Academic ",
+                status: "Learning",
+                image: "https://www.uit.edu/assets/images/faculty/ZahirSir.jpg"
+            },
+            {
+                name: "DR. ABID KARIM",
+                positioin: "Head Project based  ",
+                status: "Learning",
+                image: "https://www.uit.edu/assets/images/faculty/ZahirSir.jpg"
+            },
+
+        ]
+
+
+
+    },
+    qualityAndResearch: [
+        {
+
+            name: "researchGroup",
+            reSearchGroupScience: ["https://www.uit.edu/Report%20-%20CS%20Faculty%20Research%20Group-May%202016.doc"],
+            reSearchGroupElectricalEnginnering: ["https://www.uit.edu/Report%20-%20CS%20Faculty%20Research%20Group-May%202016.doc"],
+        },
+        {
+
+            name: "MERL",
+            about: "MERL acronym for Micro Electronics Research Labs, housed in different universities in Pakistan. Merl's Vision is to Grow/Build the semiconductor business & industry in Pakistan.",
+            reSearchGroupScience: ["https://www.uit.edu/Report%20-%20CS%20Faculty%20Research%20Group-May%202016.doc"],
+            reSearchGroupElectricalEnginnering: ["https://www.uit.edu/Report%20-%20CS%20Faculty%20Research%20Group-May%202016.doc"],
+        },
+        {
+
+            name: "Quality Enhancement Cell",
+            about: "Quality is an inherent and distinguished characteristic through which an institution can guarantee the maintenance and enhancement of its education.Quality is an ongoing process of building and sustaining relationships by assessing, anticipating, and implied needs.Educational standards are Enhanced and maintained through quality of education.",
+        },
+
+
+    ],
+    facilities: [
+
+    ]
+    ,
+    alumini: [
+
+    ],
+
+    featuredLinks: [],
+    social: [
+        {
+            name: "Facebook",
+            link: "https://www.facebook.com/UIT.Official1/",
+
+        },
+        {
+            name: "Linked",
+            link: "https://www.linkedin.com/groups/8552635",
+
+        }
+        ,
+        {
+            name: "InstaGram",
+            link: "https://www.instagram.com/groups/8552635",
+
+        }
+        ,
+        {
+            name: "Twitter",
+            link: "https://www.twitter.com/groups/8552635",
+
+        }
+    ]
+}
